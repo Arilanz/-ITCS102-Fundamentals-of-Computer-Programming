4f7501231b3995e4326b2f841e0c4fe3e314cb2f
Lanz <lanceroan3007@gmail.com>
2026-08-17 19:21:53 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,43 @@
+#variables
+
+#int
+age= 18
+birth_year=2008
+siblings= 2
+favorite=30
+
+#float
+weigth=45.5
+height=167.64
+bmi=16.7
+needed_bmi=1.8
+
+#string
+username= 'Lanz'
+password= 'mahal_ko_siya'
+job= 'unemployed'
+bmi1= 'underweight'
+
+#boolean
+normal=False
+underweight=True
+unemployed=True
+isage=False
+print('Username:',username)
+print('Password:',password)
+print('Does this user have a job? :',job)
+print("This user is",age,"and his birthyear is",birth_year,"and have",siblings,"siblings","and his favorite number is",favorite)
+print('Lanz weight is',weigth,'kg','and height is',height,'cm')
+print('Lance bmi:',bmi)
+print('The normal bmi is 18.5 so Lanz need',needed_bmi,'to be normal')
+print('Does this user is unemployed',unemployed)
+print('Does this user BMI is normal:',normal)
+print('Does this user BMI is underweight:',underweight)
+print('Does this user is 17:',isage)
+
+
+
+
+
+
+
