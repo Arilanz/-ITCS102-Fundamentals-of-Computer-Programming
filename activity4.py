58c6893ab3100ccff09055ad5e2d74a298ed80d6
Lanz <lanceroan3007@gmail.com>
2026-08-24 21:49:23 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,11 @@
+#escape sequence
+
+#\n
+#\t
+#\b
+#\r
+#\\
+#\"
+
+phrase1='\"1,2,3 Go\rkanya kanya na ito,\n\t\tKaya mo naman pala,\n\t\t\tPero bat pag dating sakin,\n\tHirap na hirap ka!!, aray ko\"'
+print(phrase1)
