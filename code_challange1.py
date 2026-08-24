95890ec3a03090885fbe0df1625c6537a35bfb41
Lanz <lanceroan3007@gmail.com>
2026-08-24 21:50:25 +0800

    Add files via upload

---
+++ code_challange1.py
@@ -0,0 +1,4 @@
+#code_challange1
+
+house='\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t**----------------------------------------------**\n\t\t\t\t\t\t\t**\t\t\t\t\t\t**\n\t\t\t\t\t\t\t**\t\t\t\t\t\t**\n\t\t\t\t\t\t\t**\t\tMy Name Is Lance\t------\t**\n\t\t\t\t\t\t\t**\t\tBSIT-1B PO,>-.->\t|\t**\n\t\t\t\t\t\t\t**\t\t\t\t\t|\t**\n\t\t\t\t\t\t\t**\t\t\t\t\t|\t**\n\t\t\t\t\t\t\t**\t\t\t\t\t|\t**\n\t\t\t\t\t\t\t**************************************************\n\t\t\t\t\t\t\t**************************************************'
+print(house)
