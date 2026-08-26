118b975084c8195cc39be6ad190bd5ea69e50c78
Lanz <lanceroan3007@gmail.com>
2026-08-26 21:30:57 +0800

    Add files via upload

---
+++ codechallanges2.py
@@ -0,0 +1,18 @@
+money=input('Amount To Be Deposite:')
+print('The amount of money recieved:',money)
+print('1000:',int(money)//1000)
+print('500:',int(money)%1000//500)
+print('200:',int(money)%1000%500//200)
+print('100:',int(money)%1000%500%200//100)
+print('50:',int(money)%1000%500%200%100//50)
+print('20:',int(money)%1000%500%200%100%50//20)
+print('10:',int(money)%1000%500%200%100%50%20//10)
+print('5:',int(money)%1000%500%200%100%50%20%10//5)
+print('1:',int(money)%1000%500%200%100%50%20%10//1)
+
+
+
+
+
+
+
