e46858c643a9d5c95db6581a4e4f71a1c762f431
Lanz <lanceroan3007@gmail.com>
2026-08-26 21:30:40 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,19 @@
+#operators
+
+#+
+#-
+#*
+#/
+#%
+#**
+#//
+
+a=1
+b=30
+
+print('The sum of',a,'and',b,'is',a+b)
+print('The difference of',a,'minus',b,'is',a-b)
+print('The product of',a,'x',b,'is',a*b)
+print('The quotient',a,'divided',b,'is',a/b)
+print('The modulus of',a,'and',b,'is',a%b)
+print('The exponent of',a,'and',b,'is',a**b)
