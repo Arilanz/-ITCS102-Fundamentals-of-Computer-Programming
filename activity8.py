ca9dfc12f6f92a002bf972a2270f4cf6456d1fb4
Lanz <lanceroan3007@gmail.com>
2026-09-04 19:44:37 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,13 @@
+#activity 8
+
+food=''
+
+a=input('Enter Your Favorite Food:')
+food += a+','
+b=input('Enter Another Food:')
+food += b+','
+c=input('Enter Another Food:')
+food += c+','
+d=input('Enter Another Food:')
+food += d+','
+print('My Favorite food is the following:',food)
