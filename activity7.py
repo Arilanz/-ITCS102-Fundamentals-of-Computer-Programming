4d705a9e936b87f34793327abcfec44bf74c800e
Lanz <lanceroan3007@gmail.com>
2026-09-04 19:44:06 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,26 @@
+#activity7 
+
+total=8
+
+a=eval(input('Enter A Number:'))
+total += a
+b=eval(input('Enter A Number:'))
+total += b
+c=eval(input('Enter A Number:'))
+total -= c
+d=eval(input('Enter A Number:'))
+total *= d
+e=eval(input('Enter A Number:'))
+total += e
+f=eval(input('Enter A Number:'))
+total /= f
+i=eval(input('Enter A Number:'))
+total -= i
+j=eval(input('Enter A Number:'))
+total -= j
+k=eval(input('Enter A Number:'))
+total += k
+l=eval(input('Enter A Number:'))
+total *= l
+print('The total is',total)
+
