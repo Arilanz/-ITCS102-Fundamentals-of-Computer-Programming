7ba147ad4bdab49b2cf208380b4f3724b4f68f65
Lanz <lanceroan3007@gmail.com>
2026-09-07 19:43:47 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,12 @@
+#Relational 
+
+print('===============VALUES==================')
+x=eval(input('Enter A Number: '))
+y=eval(input('Enter A Number: '))
+print('====================RESULT======================')
+print('The Values of',x,'is >',y,':',x > y )
+print('The Values of',x,'is <',y,':',x < y )
+print('The Values of',x,'>=',y,':',x >= y )
+print('The Values of',x,'is <=',y,':',x <= y )
+print('The Values of',x,'is =',y,':',x == y )
+print('The Values of',x,'is not =',y,':',x != y )
