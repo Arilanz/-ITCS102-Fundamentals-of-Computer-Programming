8825eebb655601482120dc8c3222216fa792701e
Lanz <lanceroan3007@gmail.com>
2026-09-14 21:49:09 +0800

    Add files via upload

---
+++ activity13.py
@@ -0,0 +1,36 @@
+#activity 13
+print('============================================')
+name=input('NAME: ')
+age=int(input('YOUR AGE: '))
+employed=bool(input('ARE YOU EMPLOYED: {IF YES ANSWER true if not press enter only}: '))
+income=eval(input('YOUR ANNUAL INCOME: '))
+credit=eval(input('CREDIT SCORE HISTORY: '))
+collateral=bool(input('DO YOU HAVE COLLATERAL: {IF YES ANSWER true if press enter only}: '))
+
+print('\n============================================')
+
+if age >= 21 and employed == True :
+    if credit >= 750 :
+        if income >= 100000:
+            print('YOU ARE ELIGABLE>_>')
+            print('INTEREST: 4.5%')
+        else:
+            print('YOU ARE ELIGABLE>_>')
+            print('5.0%')
+    elif 600 <= credit < 750:
+        if collateral == True :
+            print('YOU ARE ELIGABLE>_>')
+            print('INTEREST: 7.0%')
+        elif income < 40000:
+            print('YOU ARE ELIGABLE>_>')
+            print('INTEREST: 9.5%')
+        else:
+            print('YOU ARE ELIGABLE>_>')
+            print('INTEREST: 8.0%')
+    elif credit < 600:
+        print('YOU ARE NOT ELIGIBLE {Credit score too low} ')
+        print('SORRY........')
+else:
+        print('YOU ARE NOT ELIGIBLE {AGE OR EMPLOYMENT CRITERIA NOT MET}')
+        print('SORRY........')
+print('============================================')
