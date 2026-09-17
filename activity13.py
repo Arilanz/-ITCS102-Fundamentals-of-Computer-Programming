cecf8bc22d9c9053d7662f2c6868791e2783ff9b
Lanz <lanceroan3007@gmail.com>
2026-09-17 20:55:25 +0800

    activity13.py

--- activity13.py
+++ activity13.py
@@ -19,17 +19,17 @@ if u == username and p == password:
     age=int(input('YOUR AGE: '))
     employed=bool(input('ARE YOU EMPLOYED: {IF YES ANSWER true if not press enter only}: '))
     if employed == True:
-        job_description=input('JOB DESCRIPTION: ')
-        income=eval(input('YOUR ANNUAL INCOME: '))
-        credit=eval(input('CREDIT SCORE HISTORY: '))
-        collateral=bool(input('DO YOU HAVE COLLATERAL: {IF YES ANSWER true if press enter only}: '))
-        type=('DESCRIPTION OF COLLATERAL: {eg. HOUSE, CAR, LAND, ETC}: ')
-        if collateral == True:
-            money=int(input('VALUE OF COLLATERAL: '))
+            job_description=input('JOB DESCRIPTION: ')
+            income=eval(input('YOUR ANNUAL INCOME: '))
+            credit=eval(input('CREDIT SCORE HISTORY: '))
+            collateral=bool(input('DO YOU HAVE COLLATERAL: {IF YES ANSWER true if press enter only}: '))
             amount_loan=int(input('AMOUNT OF LOAN REQUESTED: '))
-        else:
-            print('NO COLLATERAL, PLEASE PROCEED')
-    if  age >= 21 and age <= 65 and employed == True and money >= 30000:
+            type=('DESCRIPTION OF COLLATERAL: {eg. HOUSE, CAR, LAND, ETC}: ')
+            if collateral == True and money >= 30000:
+                money=int(input('VALUE OF COLLATERAL: '))
+            else:
+                print('NO COLLATERAL, PLEASE PROCEED')
+    if  age >= 21 and age <= 65 and employed == True :
         if credit >= 750 :
             if income >= 100000:
                 print('=========================================================================')
@@ -37,14 +37,14 @@ if u == username and p == password:
                 print('INTEREST: 4.5%')
                 print('\n==========================LOAN SUMMARY==============================')
                 print('INTEREST AMOUNT: ₱', amount_loan * 0.045)
-                print('TOTAL REPAYMENT: ₱', amount_loan + (amount_loan * 0.045))
+                print('TOTAL PAYMENT: ₱', amount_loan + (amount_loan * 0.045))
             else:
                 print('=========================================================================')
                 print('YOU ARE ELIGABLE>_>')
                 print('INTEREST: 5.0%')
                 print('\n==========================LOAN SUMMARY==============================')
                 print('INTEREST AMOUNT: ₱', amount_loan * 0.05)
-                print('TOTAL REPAYMENT: ₱', amount_loan + (amount_loan * 0.05))
+                print('TOTAL PAYMENT: ₱', amount_loan + (amount_loan * 0.05))
         elif 600 <= credit < 750:
             if collateral == True :
                 print('=========================================================================')
@@ -52,21 +52,21 @@ if u == username and p == password:
                 print('INTEREST: 7.0%')
                 print('\n==========================LOAN SUMMARY==============================')
                 print('INTEREST AMOUNT: ₱', amount_loan * 0.07)
-                print('TOTAL REPAYMENT: ₱', amount_loan + (amount_loan * 0.07))
+                print('TOTAL PAYMENT: ₱', amount_loan + (amount_loan * 0.07))
             elif income < 40000:
                 print('=========================================================================')
                 print('YOU ARE ELIGABLE>_>')
                 print('INTEREST: 9.5%')
                 print('\n==========================LOAN SUMMARY==============================')
                 print('INTERST AMOUNT: ₱', amount_loan * 0.095)
-                print('TOTAL REPAYMENT: ₱', amount_loan + (amount_loan * 0.095))
+                print('TOTAL PAYMENT: ₱', amount_loan + (amount_loan * 0.095))
             else:
-                print('=========================================================================')
+                print('=========================================================================') 
                 print('YOU ARE ELIGABLE>_>')
                 print('INTEREST: 8.0%')
                 print('\n==========================LOAN SUMMARY==============================')
                 print('INTEREST AMOUNT: ₱', amount_loan * 0.08)
-                print('TOTAL REPAYMENT: ₱', amount_loan + (amount_loan * 0.08))
+                print('TOTAL PAYMENT: ₱', amount_loan + (amount_loan * 0.08))
         elif credit < 600:
             print('=========================================================================')
             print('YOU ARE NOT ELIGIBLE {Credit score too low} ')
